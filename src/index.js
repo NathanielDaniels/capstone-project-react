4c5ab682e7e48be3eb83d20317c81789a1ab97d4
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-20 21:04:45 -0600

    working on final deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {HashRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom"
+// import {HashRouter as Router} from "react-router-dom"
 
 import {ContextProvider} from "./Context"
 import App from "./App"
